a2e441de0751cd738754bd22a50ae35755d5d641
Oleksandr Rozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2022-07-11 20:20:31 +0300

    letter count task added

---
+++ letter_count/index.js
@@ -0,0 +1,26 @@
+const submit = document.getElementById("submit");
+const inputWord = document.getElementById("input-word");
+const inputLetter = document.getElementById("input-letter");
+const outputResult = document.getElementById("result");
+
+inputLetter.addEventListener("change", e => {
+  if (e.target.value.length > 1) {
+    e.target.value = e.target.value[0];
+  }
+});
+
+submit.addEventListener("click", e => {
+  e.preventDefault();
+
+  const letter = inputLetter.value;
+  const word = inputWord.value;
+
+  let i = 0;
+  count = 0;
+  do {
+    count = letter === word[i] ? count+1 : count;
+    i++;
+  } while ( i < word.length);
+
+  outputResult.innerText = count;
+});
