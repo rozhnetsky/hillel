665243bd5349335dd8ab3f020e54eb22028f6d8f
Alex <a.rozhnetsky@gmail.com>
2022-07-23 14:06:51 +0300

    Palindrome check task added

---
+++ palyndrome/index.js
@@ -0,0 +1,20 @@
+const submit = document.getElementById("submit");
+const inputWord = document.getElementById("input-word");
+const outputResult = document.getElementById("result");
+
+const findPalindrome = input => {
+  for (let i = 0; i < input.length / 2; i++) {
+    let comparedIndex = input.length - i - 1;
+    if (input[i] !== input[comparedIndex]) return false;
+  }
+  return true;
+};
+
+
+submit.addEventListener("click", e => {
+  e.preventDefault();
+
+  const isPalindrome = findPalindrome(inputWord.value);
+
+  outputResult.innerText = isPalindrome ? `Yep! ${inputWord.value} is palindrome` : `Sorry, ${inputWord.value} is not a palindrome`;
+});
