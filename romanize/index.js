f7a8c2008e2fae1e86bfc1755e3a801cef10df39
Alex <a.rozhnetsky@gmail.com>
2022-07-23 21:33:36 +0300

    improved romanize task

--- romanize/index.js
+++ romanize/index.js
@@ -1,18 +1,22 @@
 const units = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
 const tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+const hundreds = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+const thousands = ["","M","MM","MMM"];
 
 const romanize = num => {
     let outputResult;
     if (isNaN(num)) return "Not a number";
-    if (num <= 99) {
-        let tenth = Math.floor(num / 10);
+    if (num <= 2999) {
         let unit = num % 10;
-        outputResult = tens[tenth] + units[unit];
+        let tenth = Math.floor(num / 10) < 10 ? Math.floor(num / 10) : Math.floor(num / 10) % 10;
+        let hundred = Math.floor(num / 100) < 10 ? Math.floor(num / 100) : Math.floor(num / 100) % 10;
+        let thousand = Math.floor(num / 1000) < 10 ? Math.floor(num / 1000) : Math.floor(num / 1000) % 10;
+        outputResult = thousands[thousand] + hundreds[hundred] + tens[tenth] + units[unit];
         return outputResult;
     } else return "Sorry! Out of range"
 }
 
-console.log(romanize("aaa"));
+console.log(romanize(1989));
 
 console.log(romanize(1));
 
@@ -37,3 +41,5 @@ console.log(romanize(35));
 console.log(romanize(99));
 
 console.log(romanize(100));
+
+console.log(romanize(2021));
