9ac60f08c191ee6d6f771d277ab4d0c9f9c81631
Alex <a.rozhnetsky@gmail.com>
2022-07-23 23:31:41 +0300

    fix

--- palyndrome/index.js
+++ palyndrome/index.js
@@ -3,11 +3,11 @@ const inputWord = document.getElementById("input-word");
 const outputResult = document.getElementById("result");
 
 const findPalindrome = input => {
-  if (input.length % 2 > 0 || !isNaN(input) ) return false
+  if (!isNaN(input) ) return false
   for (let i = 0; i < input.length / 2; i++) {
     let comparedIndex = input.length - i - 1;
     if (input[i] !== input[comparedIndex]) return false;
-  }
+  };
   return true;
 };
 
