3887b88b864a519a9a91ae011b0429e659810542
Oleksandr Rozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2022-07-26 14:28:21 +0300

    fixes

--- guessing_game/index.js
+++ guessing_game/index.js
@@ -11,18 +11,21 @@ const guessingGame = plays => {
     let randomNumber = Math.floor(Math.random() * 10);
     let i = 0;
     do {
-        let number = parseInt(prompt("Enter num"));
-        if (number && win === true) {
-            console.log(messages.answered);
-        } else {
-            if (number > randomNumber) console.log(messages.high);
-            if (number < randomNumber) console.log(messages.low);
-            if (number === randomNumber) {
-                console.log(messages.success);
-                win = true;
+        let number = parseInt(prompt("Enter number from 1 to 10"));
+        if (isNaN(number) || number < 1 || number > 10)
+            console.log("Wrong number. Please enter number from 1 to 10");
+        else {
+            if (number && win === true) console.log(messages.answered);
+            else {
+                if (number > randomNumber) console.log(messages.high);
+                if (number < randomNumber) console.log(messages.low);
+                if (number === randomNumber) {
+                    console.log(messages.success);
+                    win = true;
+                }
             }
+            i++;
         }
-        i++;
     } while (i < plays);
     if (win === false)
         console.log(messages.stop);
