03e745d4b496831c7cd848430c322b8c48f204ff
Oleksandr Rozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2022-08-08 21:39:41 +0300

    Simple burger added

---
+++ burgers_simple/index.js
@@ -0,0 +1,72 @@
+const HAMBURGER = {
+    size: {
+        small: [5, 20],
+        large: [10, 40]
+    },
+    toppings: {
+        tomato: [1.5, 10],
+        salad: [2, 5],
+        cheese: [1, 20]
+    },
+    supplements: {
+        ketchup: [1.5, 0],
+        mayonnaise: [2, 5]
+    }
+};
+
+// Hamburger Class Create
+class Hamburger {
+    constructor() {
+        this.size = HAMBURGER.size.small;
+        this.toppings = [];
+        this.supplements = [];
+        this.amount = this.size[0];
+        this.calories = this.size[1];
+    }
+
+    calc(ingredients, index) {
+        let summ = 0;
+        for(let el of ingredients){
+            summ += el[index];
+        }
+        return summ;
+    }
+
+    addToppings(ingredients) {
+        if (Array.isArray(ingredients)) {
+            for(let el of ingredients){
+                this.toppings.push(el);
+            }
+        } else this.toppings.push(ingredients);
+    }
+
+    addSupplement(ingredients) {
+        if (Array.isArray(ingredients[0])) {
+            for(let el of ingredients){
+                this.supplements.push(el);
+            }
+        } else this.supplements.push(ingredients);
+    }
+
+    getPrice() {
+        let calcToppings = this.calc(this.toppings, 0);
+        let calcSupplements = this.calc(this.supplements, 0);
+        const calcAmount = (calcToppings + calcSupplements + this.size[0]);
+        console.log ("Price: $" + calcAmount);
+    }
+
+    getCalories() {
+        let calcToppings = this.calc(this.toppings, 1);
+        let calcSupplements = this.calc(this.supplements, 1);
+        const calcCalories = (calcToppings + calcSupplements + this.size[1]);
+        console.log ("Calories: " + calcCalories);
+    }
+}
+
+const burger = new Hamburger();
+burger.addToppings([HAMBURGER.toppings.salad, HAMBURGER.toppings.tomato]);
+burger.addSupplement(HAMBURGER.supplements.mayonnaise);
+burger.getPrice();
+burger.getCalories();
+
+
