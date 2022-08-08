291e6e78ecf8850e6beadd2be2f667f2fbe2499f
Oleksandr Rozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2022-08-08 21:13:48 +0300

    Burger task added

--- burgers/index.js
+++ burgers/index.js
@@ -1,10 +1,10 @@
 let form = document.querySelector(".form");
-const burgerIllustration = document.querySelector(".burger-illustraion");
+const burgerIllustration = document.querySelector(".burger-illustration");
 
 const HAMBURGER = {
     size: {
-        large: [10, 40],
-        small: [5, 20]
+        small: [5, 20],
+        large: [10, 40]
     },
     toppings: {
         tomato: [1.5, 10],
@@ -17,18 +17,16 @@ const HAMBURGER = {
     }
 };
 
-let { size, supplements, toppings } = HAMBURGER;
+let { size } = HAMBURGER;
 let { large, small } = size;
-let { tomato, salad, cheese } = toppings;
-let { ketchup, mayonnaise } = supplements;
 
 let burgerSetup = {
-    size: small,
+    size: [small],
     toppings: [],
     supplements: []
-
 };
 
+let hamburgerIllustration = ["bread-1", "meat-1", "bread-2"];
 
 const renderForm = () => {
     let render = "";
@@ -37,20 +35,26 @@ const renderForm = () => {
         <fieldset>
             <legend>${Object.keys(HAMBURGER)[i]}</legend>`
         for(let x in Object.values(HAMBURGER)[i]){
+            const _cost = "+$" + HAMBURGER[Object.keys(HAMBURGER)[i]][x][0];
+            const _calories = HAMBURGER[Object.keys(HAMBURGER)[i]][x][1];
             const sizeGroup = Object.keys(HAMBURGER)[i] === "size";
             const inputType = sizeGroup ? "radio" : "checkbox";
             const checked = sizeGroup && x === "small" ? "checked" : "";
             render += `
                 <div class="form__row">
                     <input type="${inputType}" name="${Object.keys(HAMBURGER)[i]}" id="${x}" value="${x}" ${checked}>
-                    <label for="${x}">${x.charAt(0).toUpperCase() + x.slice(1)}</label>
+                    <label for="${x}">${x.charAt(0).toUpperCase() + x.slice(1)} 
+                      <span>(cost: ${_cost}; calories: ${_calories})</span>
+                    </label>
                 </div>`
         }
         render += `
-        </fieldset>
-        <div class="result"></div>`
+        </fieldset>`
     }
-    form.innerHTML += render;
+    form.innerHTML +=
+    `${render}
+    <div class="result"></div>
+    `;
     initCheckboxes();
 }
 
@@ -62,94 +66,96 @@ const initCheckboxes = () => {
             const targetElement = el.target;
             const group = targetElement.getAttribute("name");
             const value = targetElement.value;
+            const elementIndex = Object.keys(HAMBURGER[group]).indexOf(value);
             if(targetElement.checked) {
-                // if (value === "large") burgerSetup[group] = burgerSetup[group].filter(e => e !== "small");
-                // else if (value === "small") burgerSetup[group] = burgerSetup[group].filter(e => e !== "large");
-                burgerSetup[group].push(HAMBURGER[group][value]);
+                if (group === "size") burgerSetup[group][0] = HAMBURGER[group][value];
+                else burgerSetup[group][elementIndex] = HAMBURGER[group][value];
+
+                if (value === "large") hamburgerIllustration.push("meat-2");
+                else if (value === "small") hamburgerIllustration = hamburgerIllustration.filter(e => e !== "meat-2");
+                else hamburgerIllustration.push(value);
             }
-            else burgerSetup[group] = burgerSetup[group].filter(e => e !== value);
-            console.log(burgerSetup);
+            else {
+                burgerSetup[group][elementIndex] = undefined;
+                hamburgerIllustration = hamburgerIllustration.filter(e => e !== value);
+            }
             createBurger(burgerSetup);
+            drawBurger(hamburgerIllustration);
         });
     });
 }
 
-const drawBurger = () => {
-    const HAMBURGER_ILLUSTRATION = 
-        ["bread-1", "ketchup", "meat-1", "salad", "meat-2", "mayonnaise", "tomato", "cheese", "bread-2"];
+const drawBurger = array => {
     let render = "";
-    for(let layer in HAMBURGER_ILLUSTRATION) {render += `
-            <div class="burger-illustraion__layer burger-illustraion__layer--${HAMBURGER_ILLUSTRATION[layer]} active">
-                <img src="images/burger-${HAMBURGER_ILLUSTRATION[layer]}.png" alt="Burger layer" />
+    for(let layer of array) {
+        render += `
+            <div class="burger-illustration__layer burger-illustration__layer--${layer} active">
+                <img src="images/burger-${layer}.png" alt="Burger layer" />
             </div>
         `
     }
     burgerIllustration.innerHTML = render;
 }
 
+drawBurger(hamburgerIllustration);
 renderForm();
-drawBurger();
-
 
 // Hamburger Class Create
 class Hamburger {
     constructor(...ingredients) {
         const _ingredients = ingredients[0] || {};
-        this.size = _ingredients.size || HAMBURGER.size.small;
+        this.size = _ingredients.size;
         this.toppings = _ingredients.toppings || 0;
         this.supplements = _ingredients.supplements || 0;
-        this.amount = this.size[0];
-        this.callories = this.size[1];
-        // console.log(this.amount, this.callories);
+        this.amount = ingredients[0].amount || this.size[0][0];
+        this.calories = ingredients[0].calories || this.size[0][1];
     }
 
-    calc(ingredients) {
-        if (ingredients !== 0) {
+    calc(ingredients, index) {
+        let summ = 0;
+        if (ingredients !== 0 && ingredients !== undefined) {
             if (Array.isArray(ingredients)){
                 for(let el of ingredients){
-                    this.amount += Number(el[0]);
-                    this.callories += Number(el[1]);
+                    if (el !== undefined) {
+                        summ += el[index];
+                    }
                 }
-            } else {
-                this.amount += Number(ingredients[0]);
-                this.callories += Number(ingredients[1]);
-            }
+            } else summ += ingredients[index];
         }
+        return summ;
     }
 
-    addToppings() {
-        return this.calc(this.toppings);
+    addToppings(index) {
+        return this.calc(this.toppings, index);
     }
 
-    addSupplement() {
-        return this.calc(this.supplements);
+    addSupplement(index) {
+        return this.calc(this.supplements, index);
     }
 
     getPrice() {
-        this.addToppings();
-        this.addSupplement();
-        return "Price: $" + this.amount;
+        const calcAmount = (this.addToppings(0) + this.addSupplement(0) + this.size[0][0])
+        return "Price: $" + calcAmount;
     }
 
     getCalories() {
-        this.addToppings();
-        this.addSupplement();
-        return "Callories: " + this.callories;
+        const calcCalories = this.addToppings(1) + this.addSupplement(1) + this.size[0][1]
+        return "Calories: " + calcCalories;
     }
-};
+}
 
 
 // New Burger Create
 const createBurger = arr => {
     const result = document.querySelector(".result");
-    let burger = new Hamburger(arr);
-    console.log(burger);
+    let burger = {};
+    burger = new Hamburger(arr);
 
     result.innerHTML = "";
 
     result.innerHTML += `
-        ${burger.getCalories()}
         ${burger.getPrice()}
+        ${burger.getCalories()}
     `
 }
 
