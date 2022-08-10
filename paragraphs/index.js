23e54d59558eef939dbb5f5658d12a59253ade43
Oleksandr Rozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2022-08-10 18:23:06 +0300

    fix

--- paragraphs/index.js
+++ paragraphs/index.js
@@ -5,11 +5,22 @@ const updateParagraphs = () => {
   for(let paragraph of paragraphs) {
     let result = paragraph.innerHTML;
     let wordsCount = 0;
-    const words =     result.match(/[a-zA-Z]+'+[a-z]*|\w+\b/g);
-    const sentences = result.split(/[^a-zA-Z-,<>"=][\W\s]/g);
-    const questions = result.match(/\?/g);
-    const screamers = result.match(/!/g);
+    const words        = result.match(/[a-zA-Z]+'+[a-z]*|\w+\b/g);
+    const sentences    = result.match(/[^.]\w*[!?.][^<]/gm);
+    const questions    = result.match(/\?/g);
+    const screamers    = result.match(/!/g);
+    const newParagraphs = result.split(/[\r\n]+/g);
     const link = "<a href='https://forcemipsum.com/'>forcemipsum.com</a>";
+
+    for(let paragraph of newParagraphs) {
+      const x = paragraph + "<br />&nbsp;<br />";
+      result = result.replace(paragraph, x);
+    }
+
+    for(let sentence of sentences) {
+      const x = sentence.slice(0, -1) + "<br />";
+      if(sentence !== "") result = result.replace(sentence, x);
+    }
 
     for(let word of words) {
       const x = `<span class="highlight">${word}</span>`
@@ -19,20 +30,15 @@ const updateParagraphs = () => {
       wordsCount++;
     }
 
-    for(let sentence of sentences) {
-      const x =  "<br>" + sentence;
-      if(sentence !== "") result = result.replace(sentence, x);
-    }
-
-    for(let question of questions) {
-      const x = "&nbsp;&#129300;";
-      result = result.replace(question, x);
-    }
-
-    for(let screamer of screamers) {
-      const x = "&nbsp;&#128558";
-      result = result.replace(screamer, x);
-    }
+    // for(let question of questions) {
+    //   const x = "&nbsp;&#129300;";
+    //   result = result.replace(question, x);
+    // }
+    //
+    // for(let screamer of screamers) {
+    //   const x = "&nbsp;&#128558";
+    //   result = result.replace(screamer, x);
+    // }
     const counter = `<span>There is ${wordsCount} words in this paragraph</span>`
 
     result =
