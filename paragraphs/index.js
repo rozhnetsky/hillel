058b21a8de0eed081a89347dc5111470d2fc6bc9
Oleksandr Rozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2022-08-12 14:44:38 +0300

    fixes

--- paragraphs/index.js
+++ paragraphs/index.js
@@ -1,57 +1,34 @@
 const paragraphs = document.getElementsByTagName("p");
 
-const updateParagraphs = () => {
-  let outputResult = "";
-  for(let paragraph of paragraphs) {
-    let result = paragraph.innerHTML;
-    let wordsCount = 0;
-    const words        = result.match(/[a-zA-Z]+'+[a-z]*|\w+\b/g);
-    const sentences    = result.match(/[^.]\w*[!?.][^<]/gm);
-    const questions    = result.match(/\?/g);
-    const screamers    = result.match(/!/g);
-    const newParagraphs = result.split(/[\r\n]+/g);
-    const link = "<a href='https://forcemipsum.com/'>forcemipsum.com</a>";
+for(let paragraph of paragraphs) {
+  let render = "";
 
-    for(let paragraph of newParagraphs) {
-      const x = paragraph + "<br />&nbsp;<br />";
-      result = result.replace(paragraph, x);
-    }
+  let plainText = paragraph.innerText;
+  const sentence = new RegExp("/\?/g");
+  plainText = plainText.split(". ").join(". <br />");
+  plainText = plainText.split("! ").join("! <br />");
+  plainText = plainText.split("? ").join("? <br />");
+  plainText = plainText.replaceAll("?", "&nbsp;&#129300;");
+  plainText = plainText.replaceAll("!", "&nbsp;&#128558;");
 
-    for(let sentence of sentences) {
-      const x = sentence.slice(0, -1) + "<br />";
-      if(sentence !== "") result = result.replace(sentence, x);
-    }
+  const words = plainText.match(/[a-zA-Z]+'+[a-z]*|\w+\b/g);
+  const eightDigitsWords = words.filter(e => e.length === 8);
+  const wordsLength = words.length;
 
-    for(let word of words) {
-      const x = `<span class="highlight">${word}</span>`
-      if (word.length === 8) {
-        result = result.replace(word, x);
-      }
-      wordsCount++;
-    }
-
-    for(let question of questions) {
-      const x = "&nbsp;&#129300;";
-      result = result.replace(question, x);
-    }
-
-    for(let screamer of screamers) {
-      const x = "&nbsp;&#128558";
-      result = result.replace(screamer, x);
-    }
-    const counter = `<span>There is ${wordsCount} words in this paragraph</span>`
-
-    result =
-      `<p>
-        ${result}
-      </p>
-      ${link}
-      ${counter}`;
-    outputResult += result;
-
-    paragraph.remove();
+  for(let word of eightDigitsWords) {
+    const template = `<span class='highlight'>${word}</span>`;
+    plainText = plainText.replace(word, template)
   }
-  document.body.innerHTML += outputResult;
-}
 
-updateParagraphs();
+  const counter = `<span>There is ${words.length} words in this paragraph</span>`;
+
+  const link = "<a href='https://forcemipsum.com/'>forcemipsum.com</a>";
+
+  render =
+    `${plainText}<br />
+    &nbsp;<br />
+    ${counter}<br />
+    ${link}`;
+
+  paragraph.innerHTML = render;
+}
